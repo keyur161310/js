045a12431ff959e407c028a5340f7d4505453a44
keyur161310 <alpeshdholakiya7858@gmail.com>
2025-12-12 08:15:10 +0530

    js

---
+++ 01_basics_variables.js
@@ -0,0 +1,18 @@
+const accountId = 123009
+let accountEmail = "keyur123@gmail.com"
+var accountPassword = "123456"
+accountCity = "gujarat"
+
+// accountIdc = 2 // not allowed
+accountEmail="raju@gmail.com"
+accountPassword="986774"
+accountCity="bombay"
+
+// console.log(accountId);
+
+/*
+PREFER NOT TO USE VAR 
+BEACUSE OF ISSUE IN BLOCK SCOPE AND FUNTIONAL SCOPE
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
---
+++ 02_datatype.js
@@ -0,0 +1,8 @@
+"use strict"   
+
+// alert("hello")
+// alert(3+3) //we are using nodejs,not browser 
+
+console.log("get data hear");console.log("3+3") 
+ 
+
