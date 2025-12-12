9416bc17189cb36a96c10b1d732a03df49d62f00
keyur161310 <alpeshdholakiya7858@gmail.com>
2025-12-12 21:28:27 +0530

    conversion&opration

--- 02_datatype.js
+++ 02_datatype.js
@@ -3,6 +3,26 @@
 // alert("hello")
 // alert(3+3) //we are using nodejs,not browser 
 
-console.log("get data hear");console.log("3+3") 
- 
+console.log("get data hear");
+console.log("3+3");
+
+ let name="keyur";
+ let age=21
+ let isLoggenIn=false
+ let state;
+
+//string = ""
+//boolean = true/false
+//null = standalone value
+//bigint
+//number = 2 to power 53 
+//undefined  
+//symbol = unique
+
+
+//object
+
+console.log(typeof  null); //object
+console.log(typeof  undefined); //undefined
+
 
---
+++ 03_ConversionOpration.js
@@ -0,0 +1,28 @@
+let score = 33 //33,33abc,kd,true,false,undefine,null
+
+
+console.log(typeof (score));
+console.log(typeof score);
+
+let ValueInNumber = Number(score);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+let change = 1 // 1, 0 ,"kd", "" 
+let booleanChange = Boolean(change)
+console.log(booleanChange);
+
+let someNumber= 33
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+
+//operations
+
+console.log(3+3);
+console.log("1"+2+2);
+console.log(2+"2");
+console.log((3+4)*3%2);
+
+
+
